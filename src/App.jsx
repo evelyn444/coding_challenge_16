5ce6b1cd5c4e0e8e57b3f7bcba03bff1a3e87001
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-17 22:05:10 -0500

    fix error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import AddProductForm from,'./AddProductForm.jsx'
 
 const App = () => {
   const [products, setProducts] = useState([
-    { id: 1, name: 'Lip Gloss', price: 12, description: 'This pink shimmery gloss will leave your lips moisturized and shining all day'}
-    { id: 2, name: 'Blush', price: 10, description: 'Vibrant, bold, and unique, this blush is long lasting and perfect to complete your look'}
-    { id: 3, name: 'Mascara', price: 15, description: 'No makeup look is complete without our long lasting and lengthening mascara'}
-    {id: 4, name: 'Foundation', price: 20, description: 'With over 20 shades, our foundation formula leaves a glowy and smooth finish everytime'}
+    { id: 1, name: 'Lip Gloss', price: 12, description: 'This pink shimmery gloss will leave your lips moisturized and shining all day'};
+    { id: 2, name: 'Blush', price: 10, description: 'Vibrant, bold, and unique, this blush is long lasting and perfect to complete your look'};
+    { id: 3, name: 'Mascara', price: 15, description: 'No makeup look is complete without our long lasting and lengthening mascara'};
+    {id: 4, name: 'Foundation', price: 20, description: 'With over 20 shades, our foundation formula leaves a glowy and smooth finish everytime'};
   ]);
 
   const addProduct = (newProduct) => {
