110f718aabb0bc5d25db19703f7b5f6717fdf3d7
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-17 21:56:02 -0500

    Create ProductList component to render each ProductItem

---
+++ src/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductItem from './ProductItem.jsx'
+
+const ProductList = ({products})=> {
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map ((product) => (
+                    <ProductItem key={product.id} product={product}/>
+                ) )}
+            </ul>
+        </div>
+    );
+};
