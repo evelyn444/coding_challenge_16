f92654667bdf4a60b6b6343404db5793faa510a3
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-17 21:49:05 -0500

    Set up App component with initial product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import ProductList from './ProductList.jsx'
+import AddProductForm from,'./AddProductForm.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Lip Gloss', price: 12, description: 'This pink shimmery gloss will leave your lips moisturized and shining all day'}
+    { id: 2, name: 'Blush', price: 10, description: 'Vibrant, bold, and unique, this blush is long lasting and perfect to complete your look'}
+    { id: 3, name: 'Mascara', price: 15, description: 'No makeup look is complete without our long lasting and lengthening mascara'}
+    {id: 4, name: 'Foundation', price: 20, description: 'With over 20 shades, our foundation formula leaves a glowy and smooth finish everytime'}
+  ]);
+
+  const addProduct = (newProduct) => {
+    setProducts ([...products, {...newProduct}]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+<h1> Product Dashboard</h1>
+<ProductList products={products}/>
+<AddProductForm addProduct={addProduct}/>
+    </div>
+  );
+};
+
+  
 
 export default App
